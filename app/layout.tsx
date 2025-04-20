e2f652ddfe41e48109924e82afc861203f31cedf
Shahrul Estar <75235970+shahrulestar@users.noreply.github.com>
2025-04-20 20:56:13 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Sleep Tracker',
+  description: 'Track your sleep.',
   generator: 'v0.dev',
 }
 
